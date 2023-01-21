cf07496a9572f844435480dbefff23804d6283ed
Topbot <topbot0319@gmail.com>
2023-01-21 21:39:14 +0800

    fix the issue

--- send_form_email.php
+++ send_form_email.php
@@ -47,8 +47,7 @@ if (isset($_POST['type']) && isset($_POST['email'])) {
   }
   $email_message .= "Name: " . clean_string($name) . "\n";
   $email_message .= "Email: " . clean_string($email) . "\n";
-  header("Location: ".$redirectUrl);
-  die();
+
   // create email headers
   $headers = 'From: ' . $email . "\r\n" .
     'Reply-To: ' . $email . "\r\n" .
